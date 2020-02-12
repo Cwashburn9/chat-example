536b7e865d7935e0ecd4ae503ca654e19ed491a9
Caitlin Washburn <caitlinkwashburn@gmail.com>
2020-02-12 17:59:38 -0600

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 
 // Let’s refactor our route handler to use sendFile instead:
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 // And in index.js we print out the chat message event:
@@ -14,16 +14,14 @@ app.get('/', function (req, res) {
 //     });
 //   });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(from, msg){
-      io.emit('chat message', from, msg);
-      console.log(from,'', msg, '');
-    });
-    socket.on('is typing', function(data){
-      socket.broadcast.emit('typing', {nickname: data.nickname});
-     });
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
   });
-  
+});
+
+
+
 http.listen(3001, function () {
-    console.log('listening on *:3001');
+  console.log('listening on *:3001');
 });
